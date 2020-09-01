667e483cff50f182e495a0708fa360cc9a1409f5
Haverbeke <haverbeke@MPB.local>
2020-09-01 22:20:20 +0300

    Use Dropzone and UploadInput components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import React from "react";
+import React, { useCallback } from "react";
 import "./App.css";
 import Image from "./components/Image";
+import Dropzone from "./components/Dropzone";
+import UploadInput from "./components/UploadInput";
 import useMobileDetection from "./hooks/useMobileDetection";
 
 const images = [];
 function App() {
   const { isMobile } = useMobileDetection();
+  const onDrop = useCallback((acceptedFiles) => {
+    console.log(acceptedFiles);
+  }, []);
   return (
     <div className="App">
       <div className="container">
         <h1 className="heading">Softtech Image Gallery Case</h1>
-        {isMobile ? (
-          <button onClick={() => alert("hi")}>Upload</button>
-        ) : (
-          <div>Dropbox</div>
-        )}
+        {isMobile ? <UploadInput /> : <Dropzone onDrop={onDrop} />}
         <div className="gallery">
           {images.map((image) => (
             <Image src={image} />
