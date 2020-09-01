4f7373311ed22b3346375c8900f646adc3b49c37
Haverbeke <haverbeke@MPB.local>
2020-09-02 01:41:54 +0300

    Add multiple image upload endpoint

--- server/index.js
+++ server/index.js
@@ -9,4 +9,26 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + "-" + Date.now());
+  },
+});
+
+const upload = multer({ storage: storage });
+
+app.post("/uploadmultiple", upload.array("images", 12), (req, res, next) => {
+  const files = req.files;
+  if (!files) {
+    const error = new Error("Please choose files");
+    error.httpStatusCode = 400;
+    return next(error);
+  }
+
+  res.send(files);
+});
+
 app.listen(3005, () => console.log("Server started on port 3005"));
